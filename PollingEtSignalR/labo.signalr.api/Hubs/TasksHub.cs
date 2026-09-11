6f1cf0a4a81f98a04dbe15b6e593f910803dde1a
Samithika Pa <1834841@cegepmontpetit.ca>
2026-09-11 15:00:30 -0400

    PROGRES : SignalR
    Partie SignalR en progrès. Connexion en cours

---
+++ PollingEtSignalR/labo.signalr.api/Hubs/TasksHub.cs
@@ -0,0 +1,21 @@
+﻿using labo.signalr.api.Data;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+
+namespace labo.signalr.api.Hubs
+{
+    public class TasksHub : Hub
+    {
+        private readonly ApplicationDbContext _context;
+        public TasksHub(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task OnConnectionAsync(string connectionId)
+        {
+            await base.OnConnectedAsync();
+            await Clients.All.SendAsync("TaskList", await _context.UselessTasks.ToListAsync());
+        }
+    }
+}
--- polling-et-signalr/app/signalr/page.tsx
+++ polling-et-signalr/app/signalr/page.tsx
@@ -4,23 +4,45 @@ import React from "react";
 import { useEffect } from "react";
 import { UselessTask } from "../models/UselessTask";
 import TaskView from "../_components/tasks-view";
+import { HubConnection, HubConnectionBuilder } from "@microsoft/signalr";
 
 export default function Home() {
 
+  const [hubConnection, setHubConnection] = React.useState<HubConnection>();
   const [tasks, setTasks] = React.useState<UselessTask[]>([]);
+
+  const hubUrl = "http://localhost:5042/tasks"
 
   useEffect(() => {
       connecttohub();
     }, []);
 
   function connecttohub() {
-    let testTasks = new Array<UselessTask>(
+    // Ancien code avec tasks hardcodées
+    /* let testTasks = new Array<UselessTask>(
           { id: 1, text: "Test Task 1", completed: false },
           { id: 2, text: "Test Task 2", completed: true });
-        setTasks(testTasks);
+        setTasks(testTasks); */
     // TODO On doit commencer par créer la connexion vers le Hub
+    let newHubConnection = new HubConnectionBuilder().withUrl(hubUrl).build()
+
     // TODO On peut commencer à écouter pour les évènements qui vont déclencher des callbacks
+      newHubConnection.on('TaskList', (data) => {
+        // data a le même type que ce qui a été envoyé par le serveur
+        console.log(data);
+        setTasks(data)
+    });
+
     // TODO On doit ensuite se connecter
+    // On se connecte au Hub  
+    newHubConnection
+        .start()
+        .then(() => {
+            console.log('La connexion est active!');
+          })
+        .catch(err => console.log('Error while starting connection: ' + err));
+
+    setHubConnection(newHubConnection);
   }
 
   function onTaskToggle(id: number) {
