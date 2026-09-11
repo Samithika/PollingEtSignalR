29200f4ed540e973a112a9d4fd02bca11d1991c5
Samithika Pa <1834841@cegepmontpetit.ca>
2026-09-11 14:20:11 -0400

    FCT : Polling
    FCT : Polling

--- polling-et-signalr/app/polling/page.tsx
+++ polling-et-signalr/app/polling/page.tsx
@@ -6,6 +6,7 @@ import TaskView from "../_components/tasks-view";
 import { UselessTask } from "../models/UselessTask";
 
 export default function Home() {
+  const domain = "http://localhost:5042/api/"
 
   const [tasks, setTasks] = React.useState<UselessTask[]>([]);
 
@@ -13,25 +14,47 @@ export default function Home() {
     updateTasks();
   }, []);
 
-  function handleTaskAdd(taskName: string) {
+  async function handleTaskAdd(taskName: string) {
     // TODO On invoke la méthode pour ajouter une tâche sur le serveur (Contrôleur d'API)
+    try{
+      const response = await axios.post(`${domain}UselessTasks/Add`, null, {params : {"taskText": taskName}})
+      let tasksCopy = [...tasks]
+      tasksCopy.push(response.data)
+      setTasks(tasksCopy)
+      console.log(`Task \"${response.data.text}\" ajouté`)
+    }catch (e: any){
+      console.log(e.response.data)
+      alert("error")
+    }
+    
   }
 
-  function onTaskToggle(id: number) {
+  async function onTaskToggle(id: number) {
     // TODO On invoke la méthode pour compléter une tâche sur le serveur (Contrôleur d'API)
+    const response = await axios.get(`${domain}UselessTasks/Complete/${id}`)
 
     let tasksCopy : UselessTask[] = [...tasks];    
     tasksCopy.find(task => task.id === id)!.completed = true;
     setTasks(tasksCopy);
+
   }
 
   async function updateTasks() {
-    let testTasks = new Array<UselessTask>(
+    // Ancien code avec Tasks codées en dur
+    /* let testTasks = new Array<UselessTask>(
       { id: 1, text: "Test Task 1", completed: false },
       { id: 2, text: "Test Task 2", completed: true });
-    setTasks(testTasks);
+    setTasks(testTasks); */
+
     // TODO: Faire une première implémentation simple avec un appel au serveur pour obtenir la liste des tâches
+    const response = await axios.get(`${domain}UselessTasks/GetAll`)
+    let list : UselessTask[] = [];
+    for (let t of response.data){
+      list.push(t)
+    }
+    setTasks(list);
     // TODO: UNE FOIS QUE VOUS AVEZ TESTER AVEC DEUX CLIENTS: Utiliser le polling pour mettre la liste de tasks à jour chaque seconde
+    setTimeout(() => {updateTasks()}, 1000)
   }
 
   return (
